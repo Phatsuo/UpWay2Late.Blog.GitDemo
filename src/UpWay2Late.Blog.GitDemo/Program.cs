2e65ae6fb4dac0f50d3561de0a9a089c38539bb4
James K <github-public@katic.org>
2017-03-02 23:44:02 -0700

    Add project files.

---
+++ src/UpWay2Late.Blog.GitDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpWay2Late.Blog.GitDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
