867b63799db4c4d7138c4d42b7d9297620f3aa3d
James K <github-public@katic.org>
2017-03-03 00:11:19 -0700

    Write to console.

--- src/UpWay2Late.Blog.GitDemo/Program.cs
+++ src/UpWay2Late.Blog.GitDemo/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UpWay2Late.Blog.GitDemo
 {
@@ -9,6 +6,8 @@ namespace UpWay2Late.Blog.GitDemo
     {
         public static void Main(string[] args)
         {
+            Console.Write("Hello Git!");
+            Console.ReadLine();
         }
     }
 }
