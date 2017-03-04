12900a4f8cc1269ea897270ccf5234ec0e64cba7
James K <github-public@katic.org>
2017-03-04 01:04:11 -0700

    Added some super important new classes.

---
+++ src/UpWay2Late.Blog.GitDemo/OtherClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpWay2Late.Blog.GitDemo
+{
+    public class OtherClass
+    {
+    }
+}
